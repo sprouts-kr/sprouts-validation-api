4cd362392e28975d02cd2a95c44efa2128993614
Jinwon Seo <jwseo@live.co.kr>
2023-08-11 13:28:15 +0900

    Set message.

--- src/main/java/kr/sprouts/validation/constraints/annotation/Uuid.java
+++ src/main/java/kr/sprouts/validation/constraints/annotation/Uuid.java
@@ -16,6 +16,7 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 @Pattern(regexp = "^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$")
 @ReportAsSingleViolation
 public @interface Uuid {
+    String message() default "{sprouts.validation.constraints.Uuid.message}";
     Class<?>[] groups() default {};
     Class<? extends Payload>[] payload() default {};
 }
