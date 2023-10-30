a88ff770e96f6fe6759c30055ef22ebc1a0ea895
jinwonseo <jwseo@live.co.kr>
2023-10-30 18:13:02 +0900

    Change javax.validation to jakarta.validation.

--- src/main/java/kr/sprouts/framework/library/validation/constraints/annotation/Uuid.java
+++ src/main/java/kr/sprouts/framework/library/validation/constraints/annotation/Uuid.java
@@ -1,9 +1,10 @@
 package kr.sprouts.framework.library.validation.constraints.annotation;
 
-import javax.validation.Constraint;
-import javax.validation.Payload;
-import javax.validation.ReportAsSingleViolation;
-import javax.validation.constraints.Pattern;
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+import jakarta.validation.ReportAsSingleViolation;
+import jakarta.validation.constraints.Pattern;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
